5b92b06e852eff15f2b60ce02f5985a02420dc56
KingCruzIII <kingcruziii@live.com>
2023-12-04 23:26:22 -0600

    Things

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import Stars from "./components/Stars/Stars";
 import { Parallax } from "@react-spring/parallax";
 import Theme from "./components/Theme/Theme";
 import ContentContainer from "./components/Content/ContentContainer";
+import { styled } from "@mui/system";
+
+const StyledParalax = styled(Parallax)(() => ({
+  top: "0",
+  left: "0",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+  "&-ms-overflow-style:": {
+    display: "none",
+  },
+  scrollbarWidth: "none",
+  msOverflowStyle: "none",
+}));
 
 const App = () => {
   return (
     <Theme>
       <>
         <ContentContainer />
-        <Parallax pages={3} style={{ top: "0", left: "0" }}>
+        <StyledParalax className="potato" pages={3} style={{}}>
           <Stars pages={3} />
-        </Parallax>
+        </StyledParalax>
       </>
     </Theme>
   );
--- src/components/Content/ContentContainer.tsx
+++ src/components/Content/ContentContainer.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Button, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { SiLinkedin, SiGithub } from "react-icons/si";
-import { FaEnvelope } from "react-icons/fa";
+// import { FaEnvelope } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa";
 
 const StyledPaper = styled("div")(() => ({
   display: "flex",
   flexDirection: "column",
-  zIndex: "100",
   margin: "auto",
   padding: "2rem",
+}));
+
+const StyledWrapper = styled("div")(() => ({
+  display: "flex",
+  width: "100vw",
+  height: "100vh",
 }));
 
 const Content = styled("div")(() => ({
@@ -17,22 +22,7 @@ const Content = styled("div")(() => ({
   flexGrow: "1",
 }));
 
-const StyledWrapper = styled("div")(() => ({
-  display: "flex",
-  alignItems: "center",
-  flexDirection: "column",
-  width: "100vw",
-  height: "100vh",
-}));
-
 const Links = styled("div")(() => ({
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  flexShrink: "1",
-}));
-
-const Nav = styled("nav")(() => ({
   flexGrow: "1",
   display: "flex",
   justifyContent: "center",
@@ -41,17 +31,23 @@ const Nav = styled("nav")(() => ({
 }));
 
 const IconButton = styled("div")(({ theme }) => ({
+  "&:hover": {
+    color: theme.palette.primary.dark,
+    fill: theme.palette.primary.dark,
+  },
   color: theme.palette.primary.main,
   fill: theme.palette.primary.main,
   height: "56px",
   width: "56px",
   margin: "1rem",
+  zIndex: "100",
 }));
 
 const StyledAvatar = styled(Avatar)(() => ({
   width: 200,
   height: 200,
   marginBottom: "1rem",
+  zIndex: "100",
 }));
 
 const StyledDownloadIcon = styled(FaDownload)(() => ({
@@ -62,6 +58,7 @@ const AvatarWrapper = styled("div")(() => ({
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
+  alignItems: "center",
 }));
 
 const Intro = styled("div")(() => ({
@@ -72,7 +69,19 @@ const Intro = styled("div")(() => ({
   flexGrow: "1",
 }));
 
-const IconLink = styled("a")(() => ({}));
+const IconLink = styled("a")(() => ({
+  zIndex: "100",
+}));
+
+const StyledDownloadButton = styled(Button)<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>
+>(({ theme }) => ({
+  "&:hover": {
+    color: theme.palette.text.primary,
+    fill: theme.palette.text.primary,
+  },
+  zIndex: "100",
+}));
 
 const ContentContainer = () => {
   return (
@@ -81,9 +90,13 @@ const ContentContainer = () => {
         <Content>
           <AvatarWrapper>
             <StyledAvatar src="https://avatars.githubusercontent.com/kingcruziii" />
-            <Button variant="contained">
+            <StyledDownloadButton
+              href="https://raw.githubusercontent.com/KingCruzIII/portfolio_website/main/Cruz_Muniz_Resume.pdf"
+              target="_blank"
+              variant="contained"
+            >
               {"Resume"} <StyledDownloadIcon />
-            </Button>
+            </StyledDownloadButton>
           </AvatarWrapper>
           <Intro>
             <Typography variant="h2">Hi,</Typography>
@@ -108,23 +121,21 @@ const ContentContainer = () => {
           </Intro>
         </Content>
         <Links>
-          <Nav>
-            <IconLink
-              target="_blank"
-              href="https://www.linkedin.com/in/cruz-muniz-iii"
-            >
-              <IconButton as={SiLinkedin} />
-            </IconLink>
-            <IconLink target="_blank" href="https://github.com/KingCruzIII">
-              <IconButton as={SiGithub} />
-            </IconLink>
-            {/* <IconLink
+          <IconLink
+            target="_blank"
+            href="https://www.linkedin.com/in/cruz-muniz-iii"
+          >
+            <IconButton as={SiLinkedin} />
+          </IconLink>
+          <IconLink target="_blank" href="https://github.com/KingCruzIII">
+            <IconButton as={SiGithub} />
+          </IconLink>
+          {/* <IconLink
               target="_blank"
               // href="https://www.linkedin.com/in/cruz-muniz-iii"
             >
               <IconButton as={FaEnvelope} />
             </IconLink> */}
-          </Nav>
         </Links>
       </StyledPaper>
     </StyledWrapper>
